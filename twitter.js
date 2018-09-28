354277d560c8a001045716ad9e1c6caf3403fc40
allen-munsch <enderplux@protonmail.com>
2018-09-28 02:25:56 -0500

    formatting

--- twitter.js
+++ twitter.js
@@ -1,18 +1,19 @@
 (function() {
   const F = (e,i,a) => {e.parentNode.removeChild(e);}
-  const THESE = ['dashboard-right', 'wtf-module', 'Trends', 'DashboardProfileCard',
-                 'promoted-tweet', 'promoted', 'Footer']
-  THESE.map((e) => {
+  const G = (e) => {
     Array.from(document.getElementsByClassName(e)).map(F)
-  })
+  }
+  const THESE = ['dashboard-right', 'wtf-module', 'Trends',
+                'DashboardProfileCard','promoted-tweet',
+                'promoted', 'Footer']
+  THESE.map(G)
   const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
       if (mutation.addedNodes && mutation.addedNodes.length > 0) {
         for (let i = 0; i < mutation.addedNodes.length; i++) {
           const newNode = mutation.addedNodes[i];
-          THESE.map((e) => {
-            Array.from(document.getElementsByClassName(e)).map(F)
-          })        }
+          THESE.map(G)
+        }
       }
     });
   });
